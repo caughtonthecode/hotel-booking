b3cabba6243cfe906d68c9d14a743ed6ec89b7b1
unknown <mahadi15-14178@diu.edu.bd>
2025-05-06 15:28:51 +0600

    design single room page card,image grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes('owner');
@@ -15,6 +16,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
         </Routes>
       </div>
       <Footer />
